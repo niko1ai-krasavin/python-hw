40ff3fdf4328f6cf5bf9e88702a9f6f3cabf70fe
niko1ai-krasavin <kosn91@mail.ru>
2021-08-10 13:14:03 +0200

    Hw8 is completed

---
+++ hw8.py
@@ -0,0 +1,17 @@
+def convert(x):
+    if x.lower() != 'cancel':
+        try:
+            number = int(x)
+            result = number // 2 if number % 2 == 0 else number * 3 + 1
+            print(result)
+            convert(input())
+        except ValueError:
+            print('Failed to convert the entered text to a number.')
+            convert(input())
+    else:
+        print('Bye!')
+
+
+if __name__ == '__main__':
+    print('Hello dear user! Please insert a text below:')
+convert(input())
